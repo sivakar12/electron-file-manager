b2c6cae8f2ac1efcda4e417a2acac29f087fb7a5
Sivakar Sithamparanathan <sivakar12@outlook.com>
2018-04-23 01:08:38 +0530

    Make navigating to favorite possible

--- react/components/index.js
+++ react/components/index.js
@@ -1,2 +1,3 @@
 export { default as FileList } from './FileList'
 export { default as File } from './File'
+export { default as FavoriteItem } from './FavoriteItem'
--- react/containers/Favorites.js
+++ react/containers/Favorites.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { addFavorite } from '../actions'
-
+import { bindActionCreators } from 'redux'
+import { addFavorite, changePath } from '../actions'
+import { FavoriteItem } from '../components'
 const Favorites = props => (
     <div>
         <h2>Favorites</h2>
-        <pre>{JSON.stringify(props.favorites)}</pre>
+        {Object.values(props.favorites).map(f => (
+            <FavoriteItem name={f.name} key={f.path}
+                onClick={() => props.changePath(f.path)}/>
+        ))}
     </div>
 )
 
 const mapStateToProps = state => ({ favorites: state.favorites })
-
-export default connect(mapStateToProps)(Favorites)
+const mapDispatchToProps = dispatch => 
+    bindActionCreators({ changePath }, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites)
